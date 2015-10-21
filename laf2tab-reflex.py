e8d310608689a4c8fb65373f2d3bc5811ea8284f
boliang <boliangzhang85@gmail.com>
2015-10-21 14:56:27 -0400

    updates to correct laf.xml parsing error due to difference between CRF laf.xml and LDC laf.xml

--- laf2tab-reflex.py
+++ laf2tab-reflex.py
@@ -16,13 +16,31 @@ def laf2tab(xml, out):
     soup = BeautifulSoup(open(xml).read(), 'html.parser')
     docid = soup.doc['id']
     for ann in soup.find_all('annotation'):
-        if ann['type'] not in ['PER', 'ORG', 'LOC']:
+        # == edited by boliang == #
+        try:
+            ann_type = ann['type']  # LDC laf format.
+        except KeyError:
+            ann_type = ann.tag  # laf format creatd by CRF tagger is a little different from LDC laf format.
+
+        if ann_type not in ['PER', 'ORG', 'LOC']:
             continue
+        # ======================= #
+
+        # if ann['type'] not in ['PER', 'ORG', 'LOC']:
+        #     continue
+
         tab = '%s\t%s\t%s\t%s:%s-%s\t%s\t%s\t%s\t%s\n'
+        # out.write(tab % ('GOLD', ann['id'], ann.extent.string, docid,
+        #                  offsets[ann['start_token']][0],
+        #                  offsets[ann['end_token']][1],
+        #                  'NIL', ann['type'], 'NAM', '1.0'))
+
+        # == edited by boliang == #
         out.write(tab % ('GOLD', ann['id'], ann.extent.string, docid,
                          offsets[ann['start_token']][0],
                          offsets[ann['end_token']][1],
-                         'NIL', ann['type'], 'NAM', '1.0'))
+                         'NIL', ann_type, 'NAM', '1.0'))
+        # ======================= #
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:

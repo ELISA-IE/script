f9668b5df02ad0b4a8812d58fb7ced5c518755c9
panx27 <panx27@gmail.com>
2016-04-03 11:43:07 -0400

    updated nist version

--- laf2tab.py
+++ laf2tab.py
@@ -2,34 +2,43 @@
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
-import sys
 import os
-from bs4 import BeautifulSoup
+import xml.etree.ElementTree as ET
 
-def laf2tab(xml, out):
-    soup = BeautifulSoup(open(xml).read(), 'html.parser')
-    docid = soup.doc['id']
-    for ann in soup.find_all('annotation'):
-        try:
-            ann_type = ann['type']  # LDC laf format.
-        except KeyError:
-            ann_type = ann.tag.string  # LDC CRF tagger format
-
-        if ann_type not in ['PER', 'ORG', 'LOC']:
-            continue
-
+def laf2tab(laf):
+    res = list()
+    xobj = ET.parse(laf)
+    xdoc = xobj.find('DOC')
+    docid = xdoc.get('id')
+    for xann in xdoc.findall('ANNOTATION'):
+        annid = xann.get('id')
+        task = xann.get('task')
+        assert task == 'NE'
+        anntype = xann.get('type')
+        extent = xann.find("EXTENT")
+        beg = extent.get('start_char')
+        end = extent.get('end_char')
+        head = extent.text
         tab = '%s\t%s\t%s\t%s:%s-%s\t%s\t%s\t%s\t%s\n'
-        out.write(tab % ('GOLD', ann['id'], ann.extent.string, docid,
-                         ann.extent['start_char'], ann.extent['end_char'],
-                         'NIL', ann_type, 'NAM', '1.0'))
+        res.append(tab % ('GOLD', annid, head, docid, beg, end,
+                          'NIL', anntype, 'NAM', '1.0'))
+    return res
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
-        print 'USAGE: python laf2tab.py <input dir> <output dir>'
+        print 'USAGE: python laf2tab.py <input dir> <output tab path>'
+        sys.exit()
     else:
         indir = sys.argv[1]
-        outdir = sys.argv[2]
-        out = open(outdir, 'w')
+        outpath = sys.argv[2]
+        res = list()
         for i in os.listdir(indir):
             if i.endswith('.laf.xml'):
-                laf2tab('%s/%s' % (indir, i), out)
+                try:
+                    res += laf2tab('%s/%s' % (indir, i))
+                except:
+                    print 'Unexpected error %s: %s' % (i, sys.exc_info())
+                    continue
+        out = open(outpath, 'w')
+        for i in res:
+            out.write('%s\n' % i)

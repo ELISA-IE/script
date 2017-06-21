71661b1f08c1d116d4dc19b91f44629b4e8399c6
Boliang Zhang <zhangb8@cs.rpi.edu>
2017-06-21 09:08:20 -0400

    boliang's script added
    softlink to hardlink
    
    rm __pycache__

--- laf2tab.py
+++ laf2tab.py
@@ -1,44 +1,68 @@
-#-*- coding: utf-8 -*-
-import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
-import os
 import xml.etree.ElementTree as ET
+import argparse
+import codecs
+import os
 
-def laf2tab(laf):
-    res = list()
-    xobj = ET.parse(laf)
-    xdoc = xobj.find('DOC')
-    docid = xdoc.get('id')
-    for xann in xdoc.findall('ANNOTATION'):
-        annid = xann.get('id')
-        task = xann.get('task')
-        assert task == 'NE'
-        anntype = xann.get('type')
-        extent = xann.find("EXTENT")
-        beg = extent.get('start_char')
-        end = extent.get('end_char')
-        head = extent.text
-        tab = '%s\t%s\t%s\t%s:%s-%s\t%s\t%s\t%s\t%s'
-        res.append(tab % ('GOLD', annid, head, docid, beg, end,
-                          'NIL', anntype, 'NAM', '1.0'))
-    return res
 
-if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print 'USAGE: python laf2tab.py <input dir> <output tab path>'
-        sys.exit()
+def laf2tab(laf_root):
+    tab = []
+    doc_id = laf_root.find("DOC").get("id")
+
+    for i, ann in enumerate(laf_root.findall(".//ANNOTATION")):
+        text = ann.find("EXTENT").text
+        start_char = ann.find("EXTENT").get("start_char")
+        end_char = ann.find("EXTENT").get("end_char")
+        tag = ann.find("TAG").text
+        if tag not in ['PER', 'GPE', 'LOC', 'ORG',
+                       'per', 'gpe', 'loc', 'org']:
+            continue
+
+        tab.append('\t'.join(["laf2tab",
+                              "%s-mention-%d" % (doc_id, i),
+                              text,
+                              "%s:%s-%s" % (doc_id, start_char, end_char),
+                              'NIL',
+                              tag,
+                              'NAM',
+                              '1.0']))
+    return '\n'.join(tab)
+
+
+def write2file(tab, tab_file):
+    if not tab:
+        print('empty tab %s' % tab_file)
+        return
+    with codecs.open(tab_file, 'w', 'utf-8') as f:
+        f.write(tab)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("laf_file")
+    parser.add_argument("tab_file")
+    parser.add_argument("-d", action="store_true", default=False,
+                        help="process directory")
+
+    args = parser.parse_args()
+
+    if args.d:
+        for fn in os.listdir(args.laf_file):
+            if not fn.endswith(".laf.xml"):
+                continue
+            try:
+                laf_file = os.path.join(args.laf_file, fn)
+                tab_file = os.path.join(args.tab_file,
+                                        fn.replace(".laf.xml", '.tab'))
+
+                laf_root = ET.parse(laf_file)
+                tab = laf2tab(laf_root)
+                write2file(tab, tab_file)
+            except AttributeError as e:
+                print("error in %s" % fn, e)
     else:
-        indir = sys.argv[1]
-        outpath = sys.argv[2]
-        res = list()
-        for i in os.listdir(indir):
-            if i.endswith('.laf.xml'):
-                try:
-                    res += laf2tab('%s/%s' % (indir, i))
-                except:
-                    print 'Unexpected error %s: %s' % (i, sys.exc_info())
-                    continue
-        out = open(outpath, 'w')
-        for i in res:
-            out.write('%s\n' % i)
+        laf_root = ET.parse(args.laf_file)
+
+        tab = laf2tab(laf_root)
+
+        write2file(tab, args.tab_file)

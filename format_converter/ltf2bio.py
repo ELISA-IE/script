9bda4b2620d0e3ddf53f86478260fff772ee8efc
boliang <boliangzhang85@gmail.com>
2017-07-09 11:39:40 -0400

    ltf2bio updates: add offset assertion check; input now can be file or directory; output can be single bio or multiple bios

--- format_converter/ltf2bio.py
+++ format_converter/ltf2bio.py
@@ -1,6 +1,7 @@
 import codecs
 import os
 import argparse
+import sys
 import xml.etree.ElementTree as ET
 
 
@@ -17,8 +18,8 @@ def ltf2bio(ltf_str):
             if t_text is None:
                 t_text = ''
             t_doc_id = token[1]
-            t_start_char = int(token[2])
-            t_end_char = int(token[3])
+            t_start_char = token[2]
+            t_end_char = token[3]
 
             # get token bio tag
             sent_res.append(' '.join([t_text,
@@ -36,10 +37,17 @@ def load_ltf(ltf_str):
     doc_id = root.find('DOC').get('id')
     for seg in root.find('DOC').find('TEXT').findall('SEG'):
         sent_tokens = []
+        seg_text = seg.find('ORIGINAL_TEXT').text
+        seg_start = int(seg.get('start_char'))
+        seg_end = int(seg.get('end_char'))
         for token in seg.findall('TOKEN'):
             token_text = token.text
-            start_char = token.get('start_char')
-            end_char = token.get('end_char')
+            start_char = int(token.get('start_char'))
+            end_char = int(token.get('end_char'))
+
+            assert seg_text[start_char-seg_start:end_char-seg_start+1] == token_text, \
+                'ltf2bio load_ltf token offset error.'
+
             sent_tokens.append((token_text, doc_id, start_char, end_char))
         doc_tokens.append(sent_tokens)
 
@@ -53,37 +61,53 @@ def write2file(bio_str, out_file):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('ltf_dp', type=str,
-                        help='ltf file directory path')
-    parser.add_argument('output_fp', type=str,
-                        help='output file path')
-    parser.add_argument('--ltf_filelist_fp', type=str,
+    parser.add_argument('ltf_input', type=str,
+                        help='ltf input path')
+    parser.add_argument('output', type=str,
+                        help='output path')
+    parser.add_argument('--ltf_filelist', type=str,
                         help='ltf filelist path')
-    parser.add_argument('-s', action='store_true', default=False,
+    parser.add_argument('-s', '--separate_output',  action='store_true', default=False,
                         help='separate output')
 
     args = parser.parse_args()
 
-    if args.ltf_filelist_fp:
-        ltf_filelist = open(args.ltf_filelist_fp).read().splitlines()
-        doc_ids = [item.replace('.ltf.xml', '')
-                   for item in ltf_filelist]
+    ltf_input = args.ltf_input
+    output = args.output
+    ltf_filelist = args.ltf_filelist
+    separate_output = args.separate_output
+
+    ltf_fp = []
+    if os.path.isdir(ltf_input):
+        if args.ltf_filelist:
+            ltf_filelist = open(args.ltf_filelist).read().splitlines()
+            ltf_fp = [os.path.join(ltf_input, item)
+                      for item in ltf_filelist]
+        else:
+            ltf_fp = [os.path.join(ltf_input, item)
+                      for item in os.listdir(args.ltf_input)
+                      if '.ltf.xml' in item]
     else:
-        doc_ids = [item.replace('.ltf.xml', '')
-                   for item in os.listdir(args.ltf_dp) if 'ltf' in item]
+        ltf_fp = [ltf_input]
 
     res = []
-    for d_id in doc_ids:
-        ltf_fp = os.path.join(args.ltf_dp, d_id+'.ltf.xml')
-        assert os.path.exists(ltf_fp)
-        # print('processing: %s' % ltf_fp)
-        ltf_str = codecs.open(ltf_fp, 'r', 'utf-8').read()
-        if args.s:
-            write2file(ltf2bio(ltf_str),
-                       '%s/%s.bio' % (args.output_fp, d_id))
-        res.append(ltf2bio(ltf_str))
+    for i, filepath in enumerate(ltf_fp):
 
-    if not args.s:
-        write2file('\n\n'.join(res), args.output_fp)
+        assert os.path.exists(filepath)
 
-    print('%d files converted.' % len(res))
+        ltf_str = codecs.open(filepath, 'r', 'utf-8').read()
+        bio_str = ltf2bio(ltf_str)
+        if separate_output:
+            out_file = os.path.join(
+                output, os.path.basename(filepath).replace('.ltf.xml', '.bio')
+            )
+            write2file(bio_str, out_file)
+        res.append(bio_str)
+
+        sys.stdout.write('%d docs processed.\r' % i)
+        sys.stdout.flush()
+
+    if not separate_output:
+        write2file('\n\n'.join(res), args.output)
+
+    print('%d docs processed in total.' % len(ltf_fp))

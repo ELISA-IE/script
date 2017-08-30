7273b75d5b2b2db83f6f794cc35f190544f55f50
boliang <boliangzhang85@gmail.com>
2017-08-30 12:35:26 -0400

    can parse zipped elisa file

--- format_converter/elisa2bio.py
+++ format_converter/elisa2bio.py
@@ -1,30 +1,17 @@
 import argparse
-import codecs
 import xml.etree.ElementTree as ET
 import gzip
 import sys
 
 
-def elisa2bio(elisa_file, output_file):
+def elisa2bio(elisa_root):
     doc_num = 0
-    #
-    # parse elisa file
-    #
-    print("=> parsing elisa file..."),
-    if elisa_file.endswith('.gz'):
-        with gzip.open(elisa_file, 'rb') as f:
-            elisa_file_content = f.read()
-        root = ET.fromstring(elisa_file_content)
-    else:
-        root = ET.parse(elisa_file)
-    print("done")
-
     #
     # generate bio
     #
-    print("=> generating bio...")
+    print("=> elisa2bio generating bio...")
     bio_results = []
-    for doc in root.findall('DOCUMENT'):
+    for doc in elisa_root.findall('DOCUMENT'):
         doc_num += 1
         doc_id = doc.get('id')
         bio_resut = []
@@ -43,7 +30,7 @@ def elisa2bio(elisa_file, output_file):
                 seg_src_tokenized = [item
                                      for item in seg_src_tokenized.split(' ')
                                      if item]
-                seg_src_id = seg_src.find('ORIG_SEG_ID').text
+                seg_src_id = seg_src.get('id')
 
                 sent_indexer = 0
                 token_offset = []
@@ -56,21 +43,23 @@ def elisa2bio(elisa_file, output_file):
 
                     token_start_char = sent_indexer
                     token_end_char = token_start_char + len(token) - 1
-                    assert seg_src_orig_raw[token_start_char:token_end_char+1] == token
+                    assert seg_src_orig_raw[token_start_char:token_end_char+1] == token, \
+                        'token offset error.'
 
                     token_offset.append((seg_src_start_char+token_start_char,
                                          seg_src_start_char+token_end_char))
                     sent_indexer = token_end_char + 1
 
-                assert len(seg_src_tokenized) == len(token_offset)
+                assert len(seg_src_tokenized) == len(token_offset), \
+                    'tokenization error.'
                 bio_resut.append('\n'.join(['%s %s:%d-%d' %
                                             (seg_src_tokenized[i],
                                              doc_id,
                                              token_offset[i][0],
                                              token_offset[i][1])
                                             for i in range(len(token_offset))]))
-            except AssertionError:
-                print('\tassertion error in %s of %s' % (seg_src_id, doc_id))
+            except (AssertionError, AttributeError) as e:
+                print('\t%s in %s: ' % (seg_src_id, doc_id), e)
                 continue
 
         bio_results.append('\n\n'.join(bio_resut))
@@ -80,8 +69,14 @@ def elisa2bio(elisa_file, output_file):
 
     print("%d documents processed in total." % doc_num)
 
-    with codecs.open(output_file, 'w', 'utf-8') as f:
-        f.write('\n\n'.join(bio_results)+'\n')
+    bio_str = '\n\n'.join(bio_results)+'\n'
+
+    return bio_str
+
+
+def write2file(bio_str, output_file):
+    with open(output_file, 'w') as f:
+        f.write(bio_str)
 
 
 if __name__ == '__main__':
@@ -93,4 +88,19 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    elisa2bio(args.elisa_file, args.output_file)
+    #
+    # parse elisa file
+    #
+    elisa_file = args.elisa_file
+    print("=> parsing elisa file..."),
+    if elisa_file.endswith('.gz'):
+        with gzip.open(elisa_file, 'rb') as f:
+            elisa_file_content = f.read()
+        root = ET.fromstring(elisa_file_content)
+    else:
+        root = ET.parse(elisa_file)
+    print("done")
+
+    bio_str = elisa2bio(root)
+
+    write2file(bio_str, args.output_file)

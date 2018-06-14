da3abec5aae0359f9499bc6cf4dbcd1ba97a87ab
panx27 <panx27@gmail.com>
2018-06-14 16:05:49 -0400

    add lowercase option

--- format_converter/bio2dat.py
+++ format_converter/bio2dat.py
@@ -17,11 +17,15 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('bio_file')
     parser.add_argument('dat_file')
+    parser.add_argument('--lower', action='store_true',
+                        help='lowercase')
     args = parser.parse_args()
 
     bio_str = codecs.open(args.bio_file, 'r', 'utf-8').read()
 
     dat = bio2dat(bio_str)
+    if args.lower:
+        dat = dat.lower()
 
     with open(args.dat_file, 'w') as f:
         f.write(dat)
